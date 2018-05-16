06addf5b41ffb56d5ffad536084bc030840c5d7b
tobi3344 <tobias.klenke@students.fhnw.ch>
2018-05-16 15:37:16 +0200

    Added separate file for definitions.

---
+++ software/host/DojoTestsoftware/src/model/Definitions.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Definitions {
+	public static String fileExtensionXml = "";
+	public static String fileExtensionAd4 = "";
+	public static String fileExtensionTxt = "";
+
+	public static String fileExtensionDescriptionXml = "";
+	public static String fileExtensionDescriptionAd4 = "";
+	public static String fileExtensionDescriptionTxt = "";
+
+	public static String german = "Deutsch";
+	public static String french = "Französisch";
+	public static String english = "Englisch";
+	public static String driveName = "SANDISK";
+}
